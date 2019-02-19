01c65b72166abb47dbc92d41daba4ba702a3dfc2
Michael Irwin <mike.irwin1@gmail.com>
2019-02-19 17:03:32 +0000

    implemented security for each in MVP

--- src/main/java/stalls/TobaccoStall.java
+++ src/main/java/stalls/TobaccoStall.java
@@ -1,8 +1,16 @@
 package stalls;
 
-public class TobaccoStall extends Stall{
+import interfaces.ISecurity;
+import visitors.Visitor;
+
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, String parkingSpot) {
         super(name, ownerName, parkingSpot);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        return (visitor.getAge() > 18);
     }
 }
--- src/test/java/stallsTest/TobaccoStallTest.java
+++ src/test/java/stallsTest/TobaccoStallTest.java
@@ -3,16 +3,21 @@ package stallsTest;
 import org.junit.Before;
 import org.junit.Test;
 import stalls.TobaccoStall;
+import visitors.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     private TobaccoStall tobaccoStall;
+    private Visitor visitor;
+    private Visitor visitor2;
 
     @Before
     public void before(){
         tobaccoStall = new TobaccoStall("Iron Lung", "Mr Tar", "Smokehouse");
+        visitor = new Visitor(10, 180, 100);
+        visitor2 = new Visitor(19, 180, 100);
     }
 
     @Test
@@ -28,5 +33,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot(){
         assertEquals("Smokehouse", tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void visitorIsNotAllowed(){
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorIsAllowed(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor2));
     }
 }
