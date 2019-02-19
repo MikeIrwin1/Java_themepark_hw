7e97a9543dc5f971c894dd51011ca0939a499993
Michael Irwin <mike.irwin1@gmail.com>
2019-02-19 16:17:53 +0000

    candyflossstall set up

--- src/test/java/stallsTest/CandyFlossStallTest.java
+++ src/test/java/stallsTest/CandyFlossStallTest.java
@@ -1,6 +1,32 @@
 package stallsTest;
 
-import stalls.Stall;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.CandyFlossStall;
 
-public class CandyFlossStallTest{
+import static org.junit.Assert.assertEquals;
+
+public class CandyFlossStallTest {
+
+    private CandyFlossStall candyFlossStall;
+
+    @Before
+    public void before(){
+        candyFlossStall = new CandyFlossStall("It's So Fluffy", "Jasmine", "River Island");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("It's So Fluffy", candyFlossStall.getName());
+    }
+
+    @Test
+    public void hasOwner(){
+        assertEquals("Jasmine", candyFlossStall.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot(){
+        assertEquals("River Island", candyFlossStall.getParkingSpot());
+    }
 }
