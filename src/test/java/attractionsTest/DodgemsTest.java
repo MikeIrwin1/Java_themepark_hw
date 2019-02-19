97ab2bb7fc8943e9abe3cb58b903127d36a2ffc4
Michael Irwin <mike.irwin1@gmail.com>
2019-02-19 15:57:43 +0000

    dodgems test passes

--- src/test/java/attractionsTest/DodgemsTest.java
+++ src/test/java/attractionsTest/DodgemsTest.java
@@ -1,4 +1,23 @@
 package attractionsTest;
 
+import attractions.Dodgems;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DodgemsTest {
+
+    private Dodgems dodgems;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Destruction Derby");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Destruction Derby", dodgems.getName());
+    }
+
 }
